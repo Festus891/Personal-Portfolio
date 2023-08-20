eda2499b476265123652e568f10372eeda7b791b
festus891 <festus891@yahoo.com>
2023-08-20 15:34:56 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import Testimonial from './components/testimonial/Testimonial'
+import Experience from './components/experience/Experience'
+import Service from './components/Service/Service'
+import Contact from './components/contact/Contact'
+import About from './components/about/About'
+import Portfolio from './components/portfolio/Porfolio'
+import Footer from './components/footer/Footer'
+
+
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Service />
+      <Portfolio />
+      <Testimonial />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/portfolio/Porfolio.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Portfolio.css'
+
+const Porfolio = () => {
+  return (
+    <div>Porfolio</div>
+  )
+}
+
+export default Porfolio
