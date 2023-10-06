2daf76ffcf56bd685af26d3f66180b6ad04cc31d
Festus891 <festus891@yahoo.com>
2023-10-06 00:10:24 -1100

    updating

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,15 +1,22 @@
-import React from 'react' 
-import CV from '../../asset/Festus_cv.pdf';
-
-
+import React from "react";
+// import CV from "../../asset/Festus_cv.pdf";
 
 const CTA = () => {
   return (
     <div className="cta">
-        <a href={CV} download className='btn'>Download Cv</a>
-        <a href="#contact" className='btn btn-primary'>Hire me</a>
+      <a
+        href="https://drive.google.com/file/d/1HRdxS5iiNBHjhxU8WCtRxMjhEdWpp8X8/view?usp=drive_link"
+        download
+        className="btn"
+        target="_blank"
+      >
+        Download Cv
+      </a>
+      <a href="#contact" className="btn btn-primary">
+        Hire me
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default CTA
+export default CTA;
